eb0e2c5ae36e5ba2d05d3e8dc8cb40b97f033dad
Alex Kofke <akofke@gmail.com>
2017-03-02 20:14:44 -0500

    initial commit
    Signed-off-by: Alex Kofke <akofke@gmail.com>

---
+++ parmed_json.py
@@ -0,0 +1,29 @@
+import json
+
+import parmed as pmd
+import numpy as np
+
+
+class ParmedEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        if hasattr(o, '__getstate__'):
+            if(isinstance(o, pmd.Structure)):
+                d = o.__getstate__()
+                d['atoms'] = o.atoms
+                return d
+            return o.__getstate__()
+        elif isinstance(o, np.ndarray):
+            return o.tolist()
+        elif o.__class__.__module__.startswith('parmed'):
+            return o.__dict__
+        else:
+            return json.JSONEncoder().default(o)
+
+
+def main():
+    gmx = pmd.load_file('test.top', xyz='test.gro')
+    print(json.dumps(gmx, cls=ParmedEncoder, indent=2))
+
+if __name__ == '__main__':
+    main()
